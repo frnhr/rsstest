e122e216497ce7e25c677a5187572ef663578adb
Fran Hrzenjak <fran@changeset.hr>
2014-08-20 01:37:31 +0200

    basic admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
+import sys
+import inspect
 from django.contrib import admin
+from api.models import Feed, Entry, Word
 
-# Register your models here.
+
+class FeedAdmin(admin.ModelAdmin):
+    model = Feed
+    
+    
+class EntryAdmin(admin.ModelAdmin):
+    model = Entry
+    
+    
+class WordAdmin(admin.ModelAdmin):
+    model = Word
+    list_filter = ['entry__feed', ]
+    
+
+# auto-register ModelAdmin classes:
+
+classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+for name, cls in classes:
+    if issubclass(cls, admin.ModelAdmin) and hasattr(cls, 'model') and 'Mixin' not in name and not issubclass(cls, admin.options.InlineModelAdmin):
+        admin.site.register(cls.model, cls)
+        
