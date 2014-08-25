f8be761ba39ac4c042d7631c52c4dd78dc8551c2
Fran Hrzenjak <fran@changeset.hr>
2014-08-25 05:00:42 +0200

    version info in header

--- rsstest/settings.py
+++ rsstest/settings.py
@@ -60,6 +60,17 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    'rsstest.context_processors.version',
+)
+
 ROOT_URLCONF = 'rsstest.urls'
 
 WSGI_APPLICATION = 'rsstest.wsgi.application'
