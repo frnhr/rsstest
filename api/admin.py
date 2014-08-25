e53bc4a76a846761440a2d175ad85fcd1ebdbb3d
Fran Hrzenjak <fran@changeset.hr>
2014-08-25 03:53:09 +0200

    quick admin tweaks

--- api/admin.py
+++ api/admin.py
@@ -1,20 +1,31 @@
 import sys
 import inspect
 from django.contrib import admin
-from api.models import Feed, Entry, Word
+from api.models import Feed, Entry, Word, WordCount
 
 
 class FeedAdmin(admin.ModelAdmin):
     model = Feed
+    list_display = ('__unicode__', 'is_active',)
+    list_filter = ('is_active',)
     
     
 class EntryAdmin(admin.ModelAdmin):
     model = Entry
+    list_display = ('__unicode__', 'feed', )
+    list_filter = ('feed',)
+    
+    
+class WordCountInline(admin.TabularInline):
+    model = WordCount
+    extra = 0
     
     
 class WordAdmin(admin.ModelAdmin):
     model = Word
-    list_filter = ['entry__feed', ]
+    list_filter = ['entry__feed', 'entry', ]
+    search_fields = ('word', )
+    inlines = (WordCountInline, )
     
 
 # auto-register ModelAdmin classes:
